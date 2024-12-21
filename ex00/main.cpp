3a0769fb6955cde986031e0c5185aceb31fde8fe
Emran BinJamaan <therealemran.design@gmail.com>
2024-12-21 15:27:34 +0400

    Modif.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,13 +4,18 @@
 #define PRINT(X) std::cout << X << "\n"
 #define FLUSH() std::cout << std::endl
 
+#define _IntVector std::vector<int>
+
+#define Vector(T) std::vector<T>
+
 int main(void) {
-  std::vector<int> vec1(4);
+  _IntVector vec1(4);
   vec1.push_back(1);
   vec1.push_back(2);
   vec1.push_back(3);
   vec1.push_back(4);
-  std::vector<int>::const_iterator it = easyfind(vec1, 5);
+
+  _IntVector::const_iterator it = easyfind(vec1, 5);
 
   if (it == vec1.end())
     ;
