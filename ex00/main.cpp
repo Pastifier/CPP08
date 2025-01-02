a2e64a9dd3b07d9f1a535338452ea4edb433bcbf
Emran BinJamaan <therealemran.design@gmail.com>
2025-01-03 03:19:40 +0400

    Made vector have exactly 4 elements

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,7 +1,8 @@
 #include "easyfind.hpp"
+#include <cstdlib>
 #include <vector>
 
-#define PRINT(X) std::cout << X << "\n"
+#define PRINT(X) std::cout << X
 #define FLUSH() std::cout << std::endl
 
 #define _IntVector std::vector<int>
@@ -9,18 +10,20 @@
 #define Vector(T) std::vector<T>
 
 int main(void) {
-  _IntVector vec1(4);
+  _IntVector vec1;
+  vec1.reserve(4);
+  vec1.end() = vec1.end() - (vec1.capacity() > 4 ? (vec1.capacity() - 4) : 0);
   vec1.push_back(1);
   vec1.push_back(2);
   vec1.push_back(3);
   vec1.push_back(4);
+  PRINT(vec1.capacity() << "\n");
 
-  _IntVector::const_iterator it = easyfind(vec1, 5);
-
-  if (it == vec1.end())
-    ;
-  else
-    PRINT(*it);
+  for (int i = -2; i < 6; ++i) {
+    _IntVector::const_iterator it = easyfind(vec1, i + 1);
+    if (it != vec1.end())
+      PRINT(*it << (i == 5 ? "" : "\n"));
+  }
   FLUSH();
   return 0;
 }
